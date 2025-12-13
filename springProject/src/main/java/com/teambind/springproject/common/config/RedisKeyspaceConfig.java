418e13bfed8cdaf742ee38b5b1fdcf6d8b7d41ee
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-13 13:41:09 +0900

    [STYLE] Reformat indentation in RedisKeyspaceConfig

--- springProject/src/main/java/com/teambind/springproject/common/config/RedisKeyspaceConfig.java
+++ springProject/src/main/java/com/teambind/springproject/common/config/RedisKeyspaceConfig.java
@@ -12,31 +12,31 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
  */
 @Configuration
 public class RedisKeyspaceConfig {
-
-  private static final String EXPIRED_EVENT_PATTERN = "__keyevent@*__:expired";
-
-  /**
-   * Redis 메시지 리스너 컨테이너 빈 설정.
-   *
-   * @param connectionFactory Redis 연결 팩토리
-   * @return RedisMessageListenerContainer 인스턴스
-   */
-  @Bean
-  public RedisMessageListenerContainer redisMessageListenerContainer(
-      final RedisConnectionFactory connectionFactory
-  ) {
-    RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-    container.setConnectionFactory(connectionFactory);
-    return container;
-  }
-
-  /**
-   * 만료 이벤트 패턴 토픽.
-   *
-   * @return PatternTopic 인스턴스
-   */
-  @Bean
-  public PatternTopic expiredEventTopic() {
-    return new PatternTopic(EXPIRED_EVENT_PATTERN);
-  }
+	
+	private static final String EXPIRED_EVENT_PATTERN = "__keyevent@*__:expired";
+	
+	/**
+	 * Redis 메시지 리스너 컨테이너 빈 설정.
+	 *
+	 * @param connectionFactory Redis 연결 팩토리
+	 * @return RedisMessageListenerContainer 인스턴스
+	 */
+	@Bean
+	public RedisMessageListenerContainer redisMessageListenerContainer(
+			final RedisConnectionFactory connectionFactory
+	) {
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		return container;
+	}
+	
+	/**
+	 * 만료 이벤트 패턴 토픽.
+	 *
+	 * @return PatternTopic 인스턴스
+	 */
+	@Bean
+	public PatternTopic expiredEventTopic() {
+		return new PatternTopic(EXPIRED_EVENT_PATTERN);
+	}
 }
