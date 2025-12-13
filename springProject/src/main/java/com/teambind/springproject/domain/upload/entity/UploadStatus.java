687752e7d8d3fb887905aa46a308ef197057626f
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-13 13:42:09 +0900

    [STYLE] Reformat indentation in UploadStatus

--- springProject/src/main/java/com/teambind/springproject/domain/upload/entity/UploadStatus.java
+++ springProject/src/main/java/com/teambind/springproject/domain/upload/entity/UploadStatus.java
@@ -4,29 +4,29 @@ package com.teambind.springproject.domain.upload.entity;
  * 업로드 상태.
  */
 public enum UploadStatus {
-
-  /**
-   * 업로드 진행 중.
-   */
-  IN_PROGRESS,
-
-  /**
-   * 업로드 완료.
-   */
-  COMPLETED,
-
-  /**
-   * 업로드 취소.
-   */
-  CANCELLED,
-
-  /**
-   * 업로드 실패.
-   */
-  FAILED,
-
-  /**
-   * 업로드 만료.
-   */
-  EXPIRED
+	
+	/**
+	 * 업로드 진행 중.
+	 */
+	IN_PROGRESS,
+	
+	/**
+	 * 업로드 완료.
+	 */
+	COMPLETED,
+	
+	/**
+	 * 업로드 취소.
+	 */
+	CANCELLED,
+	
+	/**
+	 * 업로드 실패.
+	 */
+	FAILED,
+	
+	/**
+	 * 업로드 만료.
+	 */
+	EXPIRED
 }
